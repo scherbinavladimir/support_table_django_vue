bdbe919e651fbadf09500cbf77df86601af5dde2
vvsch <vvschrus@gmail.com>
2023-07-08 12:41:10 +0300

    [+] add fe

---
+++ fe/vue.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: 'all',
+    proxy: process.env.VUE_APP_PROXY_BASE_URL,
+  },
+})
